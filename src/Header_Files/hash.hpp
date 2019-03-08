fab6030dee6c9a3d6c81fc47b8c59caf18d4bc30
KerouichaReda <kerouicha.reda@gmail.com>
2019-03-08 17:58:58 +0800

    Add External Library

---
+++ src/Header_Files/hash.hpp
@@ -0,0 +1,30 @@
+##ifndef HASH_HPP
+#define HASH_HPP
+//#include<iostream>
+#include<sstream>
+//#include<string>
+#include<iomanip>
+
+#include<openssl/sha.h>
+#include<openssl/ripemd.h>
+namespace hash
+{
+
+
+
+std::string sha256(std::string str)
+{
+ unsigned char hash[SHA256_DIGEST_LENGTH];
+ SHA256_CTX sha256;
+ SHA256_Init(&sha256);
+ SHA256_Update(&sha256, str.c_str(), str.size());
+ SHA256_Final(hash, &sha256);
+ std::stringstream ss;
+ for (int i = 0; i < SHA256_DIGEST_LENGTH; i++)
+ {
+   ss << std::hex << std::setw(2) << std::setfill('0') << (int)hash[i];
+ }
+ return ss.str();
+}
+}
+#endif
